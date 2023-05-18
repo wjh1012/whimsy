6e0a823aaee0332ae8f62b17cf95e70c2bae21d7
wangjiahang <205744581@qq.com>
2023-05-19 02:08:24 +0800

    移除不必要导包

--- whimsy-demo-rabbitmq/src/main/java/cn/wangjiahang/RabbitMqDemoApplication.java
+++ whimsy-demo-rabbitmq/src/main/java/cn/wangjiahang/RabbitMqDemoApplication.java
@@ -1,11 +1,8 @@
 package cn.wangjiahang;
 
-import cn.hutool.core.date.StopWatch;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author jh.wang
