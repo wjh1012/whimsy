1254118eeb6aeb74db9aef9e3ce2befe4f0c7739
王家航 <205744581@qq.com>
2024-05-18 14:41:50 +0800

    fix

--- whimsy-cloud-demo/authorization-6200/src/main/java/cn/wangjiahang/Authorization6200Application.java
+++ whimsy-cloud-demo/authorization-6200/src/main/java/cn/wangjiahang/Authorization6200Application.java
@@ -2,12 +2,14 @@ package cn.wangjiahang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author jh.wang
  * @since 2024/5/17
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Authorization6200Application {
     public static void main(String[] args) {
         SpringApplication.run(Authorization6200Application.class, args);
